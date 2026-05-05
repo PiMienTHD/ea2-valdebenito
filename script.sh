a3e6eca116a939754bd5b78bd26f8d16048a9cbd
PiMienTHD <adriancitovaldebenito@gmail.com>
2026-05-05 11:07:28 -0400

    Script de automatización para generar Dockerfile, construir imagen y ejecutar contenedor

--- script.sh
+++ script.sh
@@ -34,8 +34,8 @@ cat << 'EOF' > Dockerfile
 FROM python:3.9-slim
 WORKDIR /app
 RUN pip install --progress-bar off requests
-COPY app.py .
-CMD ["python", "app.py"]
+COPY apyclima.py .
+CMD ["python", "apyclima.py"]
 EOF
 
 # 5. Construcción de la imagen
